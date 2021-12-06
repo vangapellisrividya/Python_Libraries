7b3fb4b0692a9792696e664b0e08718264d786e3
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-12-06 17:56:39 +0530

    [srividya] Add: exclusive-or of an array

---
+++ Numpy/set exclusive-or.py
@@ -0,0 +1,16 @@
+'''
+    Created on Dec 5, 2021
+
+    @author:srividya
+    @Date:  5/12/2021
+    @Title: set exclusive-or
+
+    '''
+
+import numpy as np
+array1 = np.array([0, 10, 20, 40, 60, 80])
+print("Array1: ",array1)
+array2 = [10, 30, 40, 50, 70]
+print("Array2: ",array2)
+print("Unique values that are in only one (not both) of the input arrays:")
+print(np.setxor1d(array1, array2))
