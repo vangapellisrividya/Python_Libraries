a2657634aa59b867f8b91215cbf587c8de18c33a
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-12-06 17:41:15 +0530

    [srividya] Add: displaying matrix

---
+++ Numpy/3x3_matrix.py
@@ -0,0 +1,11 @@
+'''
+    Created on Dec 5, 2021
+
+    @author:srividya
+    @Date:  5/12/2021
+    @Title: 3x3 matrix using NumPy
+
+    '''
+import numpy as np
+x =  np.arange(2, 11).reshape(3,3)
+print(x)
