4e9976d442af6b8b694df2de499d14722fd2a005
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-12-06 18:02:56 +0530

    [srividya] Add: setdifference of an array

---
+++ Numpy/setdifference.py
@@ -0,0 +1,16 @@
+'''
+    Created on Dec 5, 2021
+
+    @author:srividya
+    @Date:  5/12/2021
+    @Title: setdifference
+
+    '''
+
+import numpy as np
+array1 = np.array([0, 10, 20, 40, 60, 80])
+print("Array1: ",array1)
+array2 = [10, 30, 40, 50, 70]
+print("Array2: ",array2)
+print("Unique values in array1 that are not in array2:")
+print(np.setdiff1d(array1, array2))
