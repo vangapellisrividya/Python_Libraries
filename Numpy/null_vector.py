7a5214234aa057b28feb488d180e5e453a55eb95
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-12-06 17:50:59 +0530

    [srividya] Add: creating null vector

---
+++ Numpy/null_vector.py
@@ -0,0 +1,15 @@
+'''
+    Created on Dec 5, 2021
+
+    @author:srividya
+    @Date:  5/12/2021
+    @Title: Onedimensional array using NumPy
+
+    '''
+
+import numpy as np
+x = np.zeros(10)
+print(x)
+print("Update sixth value to 11")
+x[6] = 11
+print(x)
