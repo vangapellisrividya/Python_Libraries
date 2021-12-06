e5208615ea87640f94e985fb45a86607d6f8751e
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-12-06 17:52:21 +0530

    [srividya] Add: one dimensional array

---
+++ Numpy/onedimensional_array.py
@@ -0,0 +1,14 @@
+'''
+    Created on Dec 5, 2021
+
+    @author:srividya
+    @Date:  5/12/2021
+    @Title: Onedimensional array using NumPy
+
+    '''
+
+import numpy as np
+l = [12.23, 13.32, 100, 36.32]
+print("Original List:",l)
+a = np.array(l)
+print("One-dimensional NumPy array: ",a)
